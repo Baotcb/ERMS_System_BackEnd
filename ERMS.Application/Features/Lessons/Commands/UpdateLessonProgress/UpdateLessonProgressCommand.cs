c606af4a22104a5b793529e6ac1f7b5ee653fc70
qminh3107 <satthuzombje@gmail.com>
2026-03-09 20:17:58 +0700

    feature-create and update lesson

---
+++ ERMS.Application/Features/Lessons/Commands/UpdateLessonProgress/UpdateLessonProgressCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+
+namespace ERMS.Application.Features.Lessons.Commands.UpdateLessonProgress
+{
+    public sealed class UpdateLessonProgressCommand : IRequest<bool>
+    {
+        public Guid EnrollmentId { get; set; }
+
+        public Guid LessonId { get; set; }
+
+        public int WatchPercentage { get; set; }
+
+        public int? LastPosition { get; set; }
+
+        public int TimeSpentMinutes { get; set; }
+    }
+}
