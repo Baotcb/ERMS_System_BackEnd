8995016834a5b719517d4654bd66929b159ecaab
PPz17015 <hieunuy1234@gmail.com>
2026-03-10 18:19:15 +0700

    feat:update employee

--- ERMS.Application/Features/Employees/Commands/UpdateEmployee/UpdateEmployeeCommand.cs
+++ ERMS.Application/Features/Employees/Commands/UpdateEmployee/UpdateEmployeeCommand.cs
@@ -6,11 +6,11 @@ namespace ERMS.Application.Features.Employees.Commands.UpdateEmployee
     public sealed class UpdateEmployeeCommand : IRequest<bool>
     {
         public Guid Id { get; set; }
-        public Guid EnterpriseId { get; set; }
         public int DepartmentId { get; set; }
         public string? Position { get; set; }
         public string EmploymentType { get; set; } = "FullTime";
         public Guid? ManagerId { get; set; }
         public string Status { get; set; } = "Active";
+        public string? Role { get; set; }
     }
 }
