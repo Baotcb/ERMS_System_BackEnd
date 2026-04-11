2dac019becffe9e88cba44452bc7b0fd75087611
qminh3107 <satthuzombje@gmail.com>
2026-04-11 20:27:41 +0700

    feature/ delete training plan

---
+++ ERMS.Application/Features/Training/Commands/DeleteTrainingPlan/DeleteTrainingPlanCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ERMS.Application.Features.Training.Commands.DeleteTrainingPlan
+{
+    public sealed class DeleteTrainingPlanCommand : IRequest<Guid>
+    {
+        public Guid Id { get; set; }
+    }
+}
